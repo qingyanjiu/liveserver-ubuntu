cf8407fda7acc7ce7c8a3dcb6ee7bbbef2e43cd3
Louis.L <qingyanjiu@126.com>
2016-11-25 14:20:38 +0800

    修改上传录制的视频到oss的功能，oss文件名为上传时间+流名字.flv，但暂时没有办法打关键帧,截图的视频不传到oss

--- routes/Live.js
+++ routes/Live.js
@@ -55,21 +55,25 @@ router.get('/endrecord', function (req, res, next) {
     var myDate = new Date();
     var date = myDate.pattern("yyyy-MM-dd-HH:mm:ss");
     var streamCode = req.query.name;
+    var recorder = req.query.recorder;
     var path =  req.query.path
     var co = require('co');
     var OSS = require('ali-oss')
-    var client = new OSS({
-      region: 'oss-cn-shanghai',
-      accessKeyId: 'QkCwVzn2G3St9HDo',
-      accessKeySecret: 'hsM9Sh3bTNId6ZCbea02FFXHMHygYN',
-      bucket: 'mokulive'
-    });
-    co(function* () {
-      var result = yield client.put('/videos/'+date+".flv", path);
-      console.log(result);
-    }).catch(function (err) {
-      console.log(err);
-    });
+    //截图用的视频不上传
+    if(recorder != "preview"){
+        var client = new OSS({
+          region: 'oss-cn-shanghai',
+          accessKeyId: 'QkCwVzn2G3St9HDo',
+          accessKeySecret: 'hsM9Sh3bTNId6ZCbea02FFXHMHygYN',
+          bucket: 'mokulive'
+        });
+        co(function* () {
+          var result = yield client.put('/videos/'+streamCode+"-"+date+".flv", path);
+          console.log(result);
+        }).catch(function (err) {
+          console.log(err);
+        });
+    }
 });
 
 
