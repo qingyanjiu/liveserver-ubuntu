794546dbf05e5c5c6d0fb5d033b5b57cea56ff26
root <qingyanjiu@126.com>
2017-08-01 06:12:49 +0000

    changed ttyjs port to 3001; changed start.sh shell to make sure that ttyjs and supervisor can start at same time

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 nohup /usr/local/nginx/sbin/nginx -c /nginx-1.9.0/conf/nginx.conf &
 
-/opt/node-v4.4.4-linux-x64/bin/supervisor bin/www
-tty.js --port 3001 --config /ttyjs-config.json
+nohup tty.js --config /ttyjs-config.json & 
+supervisor bin/www
+
