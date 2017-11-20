45fc06ffd36ffe55647771af78ab73df88a338c7
qingyanjiu <qingyanjiu@hotmail.com>
2017-11-20 14:13:37 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 nohup /usr/local/nginx/sbin/nginx -c /nginx-1.9.0/conf/nginx.conf &
 
-nohup tty.js --config /ttyjs-config.json & 
+#nohup tty.js --config /ttyjs-config.json & 
 supervisor bin/www
 
